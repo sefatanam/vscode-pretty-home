ce5f29da79bc65c0f0219d5dff590518c9064e77
sefatanam <sefatanam@gmail.com>
2024-07-30 23:08:16 +0600

    fix: memorize state instance

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,5 @@
 import * as vscode from "vscode";
-import { APP, openProjectInGithub, showPrettyHomeCommand, showPrettyHomeSettingsCommand } from "./lib";
+import { isTabInstanceOpen, openProjectInGithub, shouldStartInStartup, showPrettyHomeCommand, showPrettyHomeSettingsCommand } from "./lib";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -10,31 +10,20 @@ export async function activate(context: vscode.ExtensionContext) {
 	await openProjectInGithub(context);
 
 	// Open the webview by default when VS Code starts and no folder/workspace is loaded
-	vscode.window.onDidChangeWindowState((e) => {
+	vscode.window.onDidChangeWindowState((e: vscode.WindowState) => {
 		try {
-			const isPanelInstanceOpen = context.globalState.get(APP.PRETTY_HOME_PANEL_OPEN, false);
-			if (isPanelInstanceOpen) return;
+			if (isTabInstanceOpen()) return;
+			if (!shouldStartInStartup(e)) return;
+			vscode.commands.executeCommand('extension.prettyHome');
 
-			const config = vscode.workspace.getConfiguration('prettyHome');
-			const showOnStartup = config.get('showOnStartup', false);
-			const shouldOpenPrettyHome = e.active && !vscode.workspace.workspaceFolders && showOnStartup;
-			if (shouldOpenPrettyHome) {
-				vscode.commands.executeCommand('extension.prettyHome');
-			}
 		} catch (err: any) {
 			vscode.window.showErrorMessage("Something went wront to load by default.");
 		}
 	});
 
 	(function init() {
-		const isInstanceOpen = vscode.window.tabGroups.all
-			.flatMap(group => group.tabs)
-			.find(tab => tab.label.trim().includes('Pretty-Home'))
-			
-		if (!isInstanceOpen) {
-			vscode.commands.executeCommand('extension.prettyHome');
-		}
-
+		if (isTabInstanceOpen()) return;
+		vscode.commands.executeCommand('extension.prettyHome');
 	})()
 }
 
